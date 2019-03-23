b7c913e80cc3a0a13c0128be6e1554b9c998ba1f
YohannaToro <yoha_duran156@hotmail.com>
2019-03-22 19:07:33 -0500

    cambios

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -13,51 +13,59 @@ app = (function () {
                     $("#tableCinemas").append("<tr><td>" + cines[i].date
                         + "</td><td>" + cines[i].movie.name
                         + "</td><td>" + cines[i].seats.length
-                        + "</td>)
+                        + "</td>")
 
                 }
-
-
                 for(var row=0;i<10;i++) {
                     for (var i = 0; i < 10; i++) {
                         $("#tableS").append("<tr>");
                         for (var j = 0; j < 10; j++) {
                             $("#tableS").append('<td>');
-                            $("#tableS").append('<button data-row="' + i + '" data-col="' + j + '" class="btn-seat"><img data-row="' + i + '" data-col="' + j + '" src="../images/sillas2.png" /></button>')
+                            $("#tableS").append('<button id="buts" onclick="getCinemaBySeats()" on data-row="' + i + '" data-col="' + j + '" class="btn-seat"><img data-row="' + i + '" data-col="' + j + '" src="../images/sillas1.png" /></button>')
                             $("#tableS").append('</td>');
                         }
                         $("#tableS").append("</tr>");
                     }
 
                 }
-                var seats;
-                $('#tablaCinemas').on('click', 'tbody tr', function (event) {
+                
+        });
+    },getCinemaBySeats:function(){
+       
+        return apiclient.getCinemaByName(document.getElementById("search"), function (cinemas) {
+
+        var seats;
+        var cines = cinemas.functions;
+                
+                $('#butS').on('click', 'tr', function (event) {
+                    console.log("hola entre a la funcion");
                  for (var i = 0; i < cines.length ;i++) {
                                 if (cines[i].movie.name == event.target.dataset.funcion) {
                                    seats=cines[i].seats;break;
                                 }
                             }
-                }
-                for(var row=0;i<seats.length;i++) {
-                                    for (var i = 0; i < ; i++) {
-                                        $("#tableS").append("<tr>");
-                                        for (var j = 0; j < seats.length; j++) {
-                                            $("#tableS").append('<td>');
-                                            if(seats[i][j]){
-                                            $("#tableS").append('<button data-row="' + i + '" data-col="' + j + '" class="btn-seat"><img data-row="' + i + '" data-col="' + j + '" src="../images/sillas2.png" /></button>')
-                                            }else{
-                                            $("#tableS").append('<button data-row="' + i + '" data-col="' + j + '" class="btn-seat"><img data-row="' + i + '" data-col="' + j + '" src="../images/sillas2.png" /></button>')
-                                            }$("#tableS").append('</td>');
-                                        }
-                                        $("#tableS").append("</tr>");
-                                    }
-
-                                }
-                }
                 
+                
+                for(var row=0;i<seats.length;i++) {
+                    
+                        $("#tableS").append("<tr>");
+                        for (var j = 0; j < seats.length; j++) {
+                            $("#tableS").append('<td>');
+                            if(seats[i][j]){
+                            $("#tableS").append('<button data-row="' + i + '" data-col="' + j + '" class="btn-seat"><img data-row="' + i + '" data-col="' + j + '" src="../images/sillas2.png" /></button>')
+                            }else{
+                            $("#tableS").append('<button data-row="' + i + '" data-col="' + j + '" class="btn-seat"><img data-row="' + i + '" data-col="' + j + '" src="../images/sillas1.png" /></button>')
+                            }$("#tableS").append('</td>');
+                        }
+                        $("#tableS").append("</tr>");
+                    }
+
+                });
             });
-        }
+
+                
     }
+}
 
 
 })();
